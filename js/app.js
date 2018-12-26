e355708aff7641105163709b5e09f62894183c4b
LecarreGavini <lecarregavini@gmail.com>
2018-12-27 01:11:27 +0800

    Finished All Sections

--- js/app.js
+++ js/app.js
@@ -9,6 +9,24 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+function replaceImage(index) {
+  var before = document.getElementsByClassName('before');
+  var after = document.getElementsByClassName('after');
+  var headline = document.getElementsByClassName('headline');
+  before[index].style.display = "none";
+  after[index].style.display = "inline";
+  headline[index].style.color = "#FFC61A";
+}
+
+function revertImage(index) {
+  var before = document.getElementsByClassName('before');
+  var after = document.getElementsByClassName('after');
+  var headline = document.getElementsByClassName('headline');
+  before[index].style.display = "inline";
+  after[index].style.display = "none";
+  headline[index].style.color = "#000000";
+}
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
